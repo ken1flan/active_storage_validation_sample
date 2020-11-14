d81bc13086fa3ecf92dbb27adf75f34062a73113
ken1flan <ken1flan@gmail.com>
2020-11-14 22:59:54 +0900

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: 64 }
+  validates :description, presence: true, length: { maximum: 4096 }
+  validates :image, presence: true
 end
