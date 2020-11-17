c9285e16de9f3343542152cba650a82cddd32a48
ken1flan <ken1flan@gmail.com>
2020-11-17 10:01:56 +0900

    Attache multipul images to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :main_image
+  has_many_attached :other_images
 
   validates :title, presence: true, length: { maximum: 64 }
   validates :description, presence: true, length: { maximum: 4096 }
