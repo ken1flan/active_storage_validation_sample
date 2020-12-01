67cb8f7fb67256179ce1218a05df9d93fd24580f
ken1flan <ken1flan@gmail.com>
2020-12-01 09:06:00 +0900

    Fix number

--- spec/validators/attached_file_number_validator_spec.rb
+++ spec/validators/attached_file_number_validator_spec.rb
@@ -3,106 +3,93 @@ require 'rails_helper'
 RSpec.describe AttachedFileNumberValidator, type: :validator do
   subject { clazz.new(attached_file) }
 
+  let(:obj) { clazz.new }
   let(:clazz) do
     tmp_options = options # クラス定義内でletを直接参照できないため
 
-    c = Class.new do
-      include ActiveModel::Model
-
-      attr_reader :attached_file
-
-      validates :attached_file, attached_file_number: tmp_options
-
-      def initialize(attached_file)
-        @attached_file = attached_file
-      end
+    c = Class.new(ActiveRecord::Base) do
+      self.table_name = :posts
     end
-    stub_const('AttachedFileSizeValidatorTestClass', c)
+    stub_const('TestClass', c)
+
+    TestClass.has_many_attached :main_images
+    TestClass.validates :main_images, attached_file_number: tmp_options
+    TestClass
   end
 
-  let(:attached_file) do
-    attached_file = instance_double('attached_file mock')
-    allow(attached_file).to receive(:size).and_return(file_number)
-    allow(attached_file).to receive(:attached?).and_return(is_attached)
-    attached_file
-  end
-  let(:is_attached) { true }
-  let(:file_number) { 5 }
-
-  context 'without options and attached_file' do
-    let(:options) { true }
-    let(:is_attached) { false }
-
-    it { is_expected.to be_valid }
+  before do
+    file_number.times do |i|
+      obj.main_images.attach(io: File.open(Rails.root.join("spec/fixtures/file_number/file#{i}.dat")), filename: "file#{i}.dat")
+    end
   end
 
-  context 'without options, with file_number = 0' do
+  context 'with options = true, file number = 0' do
     let(:options) { true }
     let(:file_number) { 0 }
 
-    it { is_expected.to be_valid }
+    it { expect(obj).to be_valid }
   end
 
-  context 'without options, with file_number = 1' do
+  context 'with options = true, file number = 1' do
     let(:options) { true }
+    let(:file_number) { 0 }
+
+    it { expect(obj).to be_valid }
+  end
+
+  context 'with options = { maximum: 2 }, file number = 2' do
+    let(:options) { { maximum: 2 } }
+    let(:file_number) { 2 }
+
+    it { expect(obj).to be_valid }
+  end
+
+  context 'with options = { maximum: 2 }, file number = 3' do
+    let(:options) { { maximum: 2 } }
+    let(:file_number) { 3 }
+
+    it { expect(obj).to be_invalid }
+  end
+
+  context 'with options = { minimum: 2 }, file number = 1' do
+    let(:options) { { minimum: 2 } }
     let(:file_number) { 1 }
 
-    it { is_expected.to be_valid }
+    it { expect(obj).to be_invalid }
   end
 
-  context 'with options = { maximum: 3 } and file_number = 3' do
-    let(:options) { { maximum: 3 } }
-    let(:file_number) { 3 }
-
-    it { is_expected.to be_valid }
-  end
-
-  context 'with options = { maximum: 3 } and file_number = 4' do
-    let(:options) { { maximum: 3 } }
-    let(:file_number) { 4 }
-
-    it { is_expected.to be_invalid }
-  end
-
-  context 'with options = { minimum: 3 } and file_number = 3' do
-    let(:options) { { minimum: 3 } }
-    let(:file_number) { 3 }
-
-    it { is_expected.to be_valid }
-  end
-
-  context 'with options = { minimum: 3 } and file_number = 2' do
-    let(:options) { { minimum: 3 } }
+  context 'with options = { minimum: 2 }, file number = 2' do
+    let(:options) { { minimum: 2 } }
     let(:file_number) { 2 }
 
-    it { is_expected.to be_invalid }
+    it { expect(obj).to be_valid }
   end
 
-  context 'with options = { minimum: 3, maximum: 5 } and file_number = 2' do
+  context 'with options = { minimum: 3, maximum: 5 }, file number = 2' do
     let(:options) { { minimum: 3, maximum: 5 } }
     let(:file_number) { 2 }
 
-    it { is_expected.to be_invalid }
+    it { expect(obj).to be_invalid }
   end
 
-  context 'with options = { minimum: 3, maximum: 5 } and file_number = 3' do
+  context 'with options = { minimum: 3, maximum: 5 }, file number = 3' do
     let(:options) { { minimum: 3, maximum: 5 } }
     let(:file_number) { 3 }
 
-    it { is_expected.to be_valid }
+    it { expect(obj).to be_valid }
   end
 
   context 'with options = { minimum: 3, maximum: 5 } and file_number = 5' do
     let(:options) { { minimum: 3, maximum: 5 } }
     let(:file_number) { 5 }
 
-    it { is_expected.to be_valid }
+    it { expect(obj).to be_valid }
   end
 
   context 'with options = { minimum: 3, maximum: 5 } and file_number = 6' do
     let(:options) { { minimum: 3, maximum: 5 } }
     let(:file_number) { 6 }
 
-    it { is_expected.to be_invalid }
+    it { expect(obj).to be_invalid }
   end
 end
